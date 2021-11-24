992906ca45b0da73a7fb3d3ffc22f3ffe882adb9
MegMu <meghna.n21@gmail.com>
2021-11-24 12:25:49 +0530

    Initial Commit for SKU Promotion Engine
    Initial Commit for SKU Promotion Engine

---
+++ src/main/java/com/example/promotionengine/constant/EngineConstants.java
@@ -0,0 +1,13 @@
+package com.example.promotionengine.constant;
+
+public class EngineConstants {
+	
+	public static final int SKUA_SINGLE_PRICE = 50;
+	public static final int SKUA_PROMO_PRICE_FOR3 = 130;
+	public static final int SKUB_SINGLE_PRICE = 30;
+	public static final int SKUB_PROMO_PRICE_FOR2 = 45;
+	public static final int SKUC_SINGLE_PRICE = 20;
+	public static final int SKUD_SINGLE_PRICE = 15;
+	public static final int SKUC_WITH_SKUD_PROMO_PRICE = 30;
+
+}
