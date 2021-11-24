e20eb71f04f1d619539c88604250555ca7e65ce2
MegMu <meghna.n21@gmail.com>
2021-11-24 12:34:09 +0530

    Added HTTP request response beans

---
+++ src/main/java/com/example/promotionengine/bean/RequestSKU.java
@@ -0,0 +1,42 @@
+package com.example.promotionengine.bean;
+
+public class RequestSKU {
+
+	private Integer SKUA;
+	private Integer SKUB;
+	private Integer SKUC;
+	private Integer SKUD;
+
+	public Integer getSKUA() {
+		return SKUA;
+	}
+
+	public void setSKUA(Integer sKUA) {
+		SKUA = sKUA;
+	}
+
+	public Integer getSKUB() {
+		return SKUB;
+	}
+
+	public void setSKUB(Integer sKUB) {
+		SKUB = sKUB;
+	}
+
+	public Integer getSKUC() {
+		return SKUC;
+	}
+
+	public void setSKUC(Integer sKUC) {
+		SKUC = sKUC;
+	}
+
+	public Integer getSKUD() {
+		return SKUD;
+	}
+
+	public void setSKUD(Integer sKUD) {
+		SKUD = sKUD;
+	}
+
+}
---
+++ src/main/java/com/example/promotionengine/bean/TotalResponse.java
@@ -0,0 +1,15 @@
+package com.example.promotionengine.bean;
+
+public class TotalResponse {
+	
+	private Integer totalPrice;
+
+	public Integer getTotalPrice() {
+		return totalPrice;
+	}
+
+	public void setTotalPrice(Integer totalPrice) {
+		this.totalPrice = totalPrice;
+	}
+	
+}
