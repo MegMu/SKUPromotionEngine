be2409c85a7511f51677d423c87099413e66e996
MegMu <meghna.n21@gmail.com>
2021-11-24 16:15:55 +0530

    added Junit test cases
    added Junit test cases

--- src/test/java/com/example/promotionengine/PromotionEngineApplicationTests.java
+++ src/test/java/com/example/promotionengine/PromotionEngineApplicationTests.java
@@ -1,13 +1,43 @@
 package com.example.promotionengine;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.net.URI;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.util.UriComponentsBuilder;
 
-@SpringBootTest
-class PromotionEngineApplicationTests {
+import com.example.promotionengine.model.RequestSKU;
+import com.example.promotionengine.model.TotalResponse;
 
-	@Test
-	void contextLoads() {
-	}
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class PromotionEngineApplicationTests {
+  
+  @Autowired
+  private TestRestTemplate restTemplate;
+   @Test
+   @DisplayName("/PromotionEngineTest ")
+   void testMessage() {
+     
+     RequestSKU req = new RequestSKU();
+     req.setSKUA(1);
+     req.setSKUB(1);
+     req.setSKUC(1);
+     req.setSKUD(1);
+     URI targetUrl= UriComponentsBuilder.fromUriString("/promotionEngine/v1/calculateTotalPrice/")                             
+            .queryParam("request",req)                               
+            .build()                                                
+            .encode()                                               
+            .toUri();
+     
+    ResponseEntity<TotalResponse> message = this.restTemplate.postForEntity(targetUrl,req, TotalResponse.class);
+    assertThat(message.getBody().getTotalPrice()).isEqualTo(100);
+   }
 
 }
